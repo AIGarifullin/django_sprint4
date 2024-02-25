90df07dbef41b1645fdde8dd26d1f8e6d6d05b4a
Адель Гарифуллин <volpatibon@ya.ru>
2024-02-25 14:50:21 +0300

    Version 1.3 of project 'django_sprint4'

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -17,13 +17,10 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     # Регистрация нового пользователя.
     path('auth/registration/', CreateView.as_view(
-            template_name='registration/registration_form.html',
-            form_class=UserCreationForm,
-            success_url=reverse_lazy('blog:index'),
-        ),
-        name='registration',
-        ),
-]
+        template_name='registration/registration_form.html',
+        form_class=UserCreationForm,
+        success_url=reverse_lazy('blog:index')),
+        name='registration'),]
 
 # Если проект запущен в режиме разработки...
 if settings.DEBUG:
